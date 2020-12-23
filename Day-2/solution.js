daf8f323fbe1a000054c52c3decbf133bda1a964
Satish Narayan <narayansatish97@gmail.com>
2020-12-23 11:43:38 +0530

    question1

--- Day-2/solution.js
+++ Day-2/solution.js
@@ -1,9 +1,15 @@
 // write all your solution here. 
 
-function question1(){
+function question1(input){
+    return Array.isArray(input);
 
 }
+let  answer1 = question1('w3resource');
+console.log(answer1);
 
+answer1 = question1([1, 2, 4, 0]);
+console.log(answer1);
+/*
 function question2(){
 
 }
@@ -19,6 +25,7 @@ const answer2 = question2('pass sample input');
 const answer3 = question3('pass sample input');
 
 
-console.log(answer1);
+
 console.log(answer2);
 console.log(answer3);
+*/
