a20dd97cc4dca51d1a517355699a0868144893d2
Satish Narayan <narayansatish97@gmail.com>
2020-12-23 13:57:51 +0530

    question3

--- Day-2/solution.js
+++ Day-2/solution.js
@@ -9,6 +9,13 @@ console.log(answer1);
 
 answer1 = question1([1, 2, 4, 0]);
 console.log(answer1);
+////////////////////////////////////////
+function question3(s){
+    return s.map((s)=>s.substring(0, 2) + '/' + s.substring(3));
+
+}
+let answer3=question3(['08-14', '10-04', '04-21']);
+console.log(answer3);
 ///////////////////////////////
 function question4(input){
     return input.map((item)=>Math.ceil(item));
